cb7c6f8bc5c9414af3d5b6b205b02f1ccc2f37a9
YunHa <pgup10753@gmail.com>
2024-10-11 10:04:16 +0900

    comparecommits according to github docs

--- reviewer.js
+++ reviewer.js
@@ -26,18 +26,30 @@ import * as dotenv from 'dotenv';
             ? +process.env.MAX_PATCH_LENGTH
             : Infinity;
         // 두 커밋 간의 변경 사항 가져오기 (compareCommits 사용)
-        const { data } = await octokit.repos.compareCommits({
+        const { data } = await octokit.request('GET /repos/{owner}/{repo}/compare/{base}...{head}', {
             owner: owner,
             repo: repo,
-            basehead: `${base}...${head}`
-        });
+            base: base,
+            head: head
+        })
+        // const { data } = await octokit.repos.compareCommits({
+        //     owner: owner,
+        //     repo: repo,
+        //     basehead: `${base}...${head}`
+        // });
         let { files: changedFiles, commits } = data.data;
         if (commits.length >= 2) {
-            const { data: { files }, } = await octokit.repos.compareCommits({
+            const { data: { files }, } = await octokit.request('GET /repos/{owner}/{repo}/compare/{base}...{head}', {
                 owner: owner,
                 repo: repo,
-                basehead: `${commits[commits.length - 2].sha}...${commits[commits.length - 1].sha}`,
+                base: commits[commits.length - 2].sha,
+                head: commits[commits.length - 1].sha
             })
+            // const { data: { files }, } = await octokit.repos.compareCommits({
+            //     owner: owner,
+            //     repo: repo,
+            //     basehead: `${commits[commits.length - 2].sha}...${commits[commits.length - 1].sha}`,
+            // })
             const ignoreList = (process.env.IGNORE || process.env.ignore || '')
                 .split('\n')
                 .filter((v) => v !== '');
