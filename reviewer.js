42caebc08b0e2c6161e23099b887c2bd5708c8ae
YunHa <pgup10753@gmail.com>
2024-10-11 10:26:17 +0900

    headers in compareCommits

--- reviewer.js
+++ reviewer.js
@@ -26,44 +26,44 @@ import * as dotenv from 'dotenv';
             ? +process.env.MAX_PATCH_LENGTH
             : Infinity;
         // 두 커밋 간의 변경 사항 가져오기 (compareCommits 사용)
-        try {
-            const { data } = await octokit.request('GET /repos/{owner}/{repo}/compare/{base}...{head}', {
+        const { data } = await octokit.request('GET /repos/{owner}/{repo}/compare/{base}...{head}', {
+            owner: owner,
+            repo: repo,
+            basehead: `${base}...${head}`,
+            headers: {
+                'X-GitHub-Api-Version': '2022-11-28'
+            }
+        })
+        // const { data } = await octokit.repos.compareCommits({
+        //     owner: owner,
+        //     repo: repo,
+        //     basehead: `${base}...${head}`
+        // });
+        let { files: changedFiles, commits } = data.data;
+        if (commits.length >= 2) {
+            const { data: { files }, } = await octokit.request('GET /repos/{owner}/{repo}/compare/{base}...{head}', {
                 owner: owner,
                 repo: repo,
-                base: base,
-                head: head
+                basehead: `${commits[commits.length - 2].sha}...${commits[commits.length - 1].sha}`,
+                headers: {
+                    'X-GitHub-Api-Version': '2022-11-28'
+                }
             })
-            // const { data } = await octokit.repos.compareCommits({
+            // const { data: { files }, } = await octokit.repos.compareCommits({
             //     owner: owner,
             //     repo: repo,
-            //     basehead: `${base}...${head}`
-            // });
-            let { files: changedFiles, commits } = data.data;
-            if (commits.length >= 2) {
-                const { data: { files }, } = await octokit.request('GET /repos/{owner}/{repo}/compare/{base}...{head}', {
-                    owner: owner,
-                    repo: repo,
-                    base: commits[commits.length - 2].sha,
-                    head: commits[commits.length - 1].sha
-                })
-                // const { data: { files }, } = await octokit.repos.compareCommits({
-                //     owner: owner,
-                //     repo: repo,
-                //     basehead: `${commits[commits.length - 2].sha}...${commits[commits.length - 1].sha}`,
-                // })
-                const ignoreList = (process.env.IGNORE || process.env.ignore || '')
-                    .split('\n')
-                    .filter((v) => v !== '');
-                const filesNames = files?.map((file) => file.filename) || [];
-                changedFiles = changedFiles?.filter((file) => filesNames.includes(file.filename) &&
-                    !ignoreList.includes(file.filename));
-            }
-            if (!changedFiles?.length) {
-                console.log('no change found');
-                return 'no change';
-            }
-        } catch (error) {
-            console.error("Error:", error);
+            //     basehead: `${commits[commits.length - 2].sha}...${commits[commits.length - 1].sha}`,
+            // })
+            const ignoreList = (process.env.IGNORE || process.env.ignore || '')
+                .split('\n')
+                .filter((v) => v !== '');
+            const filesNames = files?.map((file) => file.filename) || [];
+            changedFiles = changedFiles?.filter((file) => filesNames.includes(file.filename) &&
+                !ignoreList.includes(file.filename));
+        }
+        if (!changedFiles?.length) {
+            console.log('no change found');
+            return 'no change';
         }
         // 변경사항이 있으면 각 변경사항마다 codeReview 진행
         for (let i = 0; i < changedFiles.length; i++){
